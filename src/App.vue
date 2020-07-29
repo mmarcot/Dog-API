0767780d24dbb601992846c4f49f678bf569c70a
mma <mma@deb.com>
2020-07-29 22:14:02 +0200

    Dogapi random working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="container-fluid p-0" id="app">
+		<div class="jumbotron py-1">
+			<h1 class="display-5">DogAPI</h1>
+		</div>
+		<DogInput v-on:search-dog="searchDog" />
+		<button class="btn btn-info my-4" v-on:click="randomDog">Random</button>
+		<img id="dog-image" class="d-block mx-auto" v-bind:src="imgSource">
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import DogInput from './components/DogInput.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'App',
+		data() {
+			return {
+				imgSource: ''
+			}
+		},
+		components: {
+			DogInput
+		},
+		methods: {
+			searchDog(payload) {
+				console.log(payload.dogInputValue);
+			},
+			randomDog() {
+				var xhttp = new XMLHttpRequest();
+				let this_comp = this;
+				xhttp.onreadystatechange = function() {
+					if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+						var response = JSON.parse(this.responseText);
+						this_comp.imgSource = response.message;
+					}
+				};
+				xhttp.open('GET', 'https://dog.ceo/api/breeds/image/random');
+				xhttp.send();
+			}
+		}
+	}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		text-align: center;
+	}
+	#dog-image {
+		max-height: 700px;
+		max-width: 700px;
+		height: auto;
+		width: auto;
+	}
 </style>
---
+++ src/components/DogInput.vue
@@ -0,0 +1,28 @@
+<template>
+	<input 
+		type="text" 
+		class="form-control w-50 mx-auto" 
+		id="dog-input" 
+		placeholder="Search" 
+		v-on:keyup.enter="searchDog"
+	/>
+</template>
+
+
+<script>
+	export default {
+		name: "DogInput",
+		methods: {
+			searchDog() {
+				let dogInput = document.getElementById('dog-input');
+				this.$emit('search-dog', { dogInputValue: dogInput.value });
+				dogInput.value = '';
+			}
+		}
+	}	
+</script>
+
+
+<style scoped lang="scss">
+	
+</style>
